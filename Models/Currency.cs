5f1a5f6e01879094957f2a80f194a6a0d0a14cb0
Hrystyna Ivaniv <khrystyna.ivaniv.shi.2024@lpnu.ua>
2025-12-28 11:49:00 +0200

    add Currency model

---
+++ Models/Currency.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CryptoViewer.Models
+{
+    public class Currency
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("symbol")]
+        public string Symbol { get; set; }
+
+        [JsonPropertyName("current_price")]
+        public decimal? CurrentPrice { get; set; }
+
+        [JsonPropertyName("price_change_percentage_24h_in_currency")]
+        public decimal? PriceChange24h { get; set; }
+
+        [JsonPropertyName("total_volume")]
+        public decimal? TotalVolume { get; set; }
+    }
+}
