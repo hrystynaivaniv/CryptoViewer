d63b8e58f69c75e3e1ca2d90afd3d7cb8b9282f9
Hrystyna Ivaniv <khrystyna.ivaniv.shi.2024@lpnu.ua>
2025-12-28 12:13:28 +0200

    Imlement MainCurrencyPage

---
+++ Views/MainCurrencyPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using CryptoViewer.ViewModels;
+
+namespace CryptoViewer.Views
+{
+    public partial class MainCurrencyPage : UserControl
+    {
+        public MainCurrencyPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
