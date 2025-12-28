ff670769b4f9c98276198e979d8a369f955b35a0
Hrystyna Ivaniv <khrystyna.ivaniv.shi.2024@lpnu.ua>
2025-12-28 12:13:44 +0200

    Imlement CurrencyDetailPage

---
+++ Views/CurrencyDetailPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using CryptoViewer.ViewModels;
+
+namespace CryptoViewer.Views
+{
+    public partial class CurrencyDetailPage : UserControl
+    {
+        public CurrencyDetailPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
